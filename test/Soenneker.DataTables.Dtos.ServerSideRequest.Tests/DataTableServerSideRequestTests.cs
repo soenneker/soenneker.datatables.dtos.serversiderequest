3422d958f9b1b19836761a7d22f3024e0fec31f7
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:34 -0500

    TUnit conversion

--- test/Soenneker.DataTables.Dtos.ServerSideRequest.Tests/DataTableServerSideRequestTests.cs
+++ test/Soenneker.DataTables.Dtos.ServerSideRequest.Tests/DataTableServerSideRequestTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.DataTables.Dtos.ServerSideRequest.Tests;
 
 public sealed class DataTableServerSideRequestTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
